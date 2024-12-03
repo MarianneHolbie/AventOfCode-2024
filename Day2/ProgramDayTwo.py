b57a1165af1161a8d067ed83e92d2489740d824e
MarianneHolbie <112870394+MarianneHolbie@users.noreply.github.com>
2024-12-03 14:35:36 +0100

    Solution for Day 2 - part Two

--- Day2/ProgramDayTwo.py
+++ Day2/ProgramDayTwo.py
@@ -26,3 +26,39 @@ with open('input.txt', 'r') as file:
             count += 1
 
 print("Safe reports:", count)
+
+
+#####################
+#     Part Two      #
+#####################
+
+def is_valid_sequence(numbers):
+
+    diffs = [numbers[i+1] - numbers[i] for i in range(len(numbers)-1)]
+
+    if all(1 <= abs(d) <= 3 for d in diffs):
+        return all(d > 0 for d in diffs) or all(d < 0 for d in diffs)
+    return False
+
+
+def is_safe_with_removal(line):
+    numbers = list(map(int, line.strip().split()))
+
+    if is_valid_sequence(numbers):
+        return True
+
+    for idx in range(len(numbers)):
+        new_numbers = numbers[:idx] + numbers[idx+1:]
+        if is_valid_sequence(new_numbers):
+            return True
+
+    return False
+
+
+count = 0
+with open('input.txt', 'r') as f:
+    for line in f:
+        if is_safe_with_removal(line):
+            count += 1
+
+print("New safe count:", count)
